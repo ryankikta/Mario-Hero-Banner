512af5a91ba89304693d59219ff46e09ddacf653
ryankikta <ryankikta@gmail.com>
2019-07-11 12:59:49 -0400

    Create hero-javascript.js

---
+++ hero-javascript.js
@@ -0,0 +1,11 @@
+//Video hide time-outs in milliseconds
+setTimeout(heroHideVid1, 5000);
+setTimeout(heroHideVid2, 10000);
+
+//Hide Video Functions
+function heroHideVid1(){
+  document.getElementById("video1").style.display="none";   
+           }
+function heroHideVid2() {
+  document.getElementById("video2").style.display="none"; 
+}
